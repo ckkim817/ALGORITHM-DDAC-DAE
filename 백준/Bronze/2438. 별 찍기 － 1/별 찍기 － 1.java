ab40f1d29bfb8150fc0d995f83eb6e79a91251f7
kyunlque <81475587+ckkim817@users.noreply.github.com>
2024-09-01 14:52:10 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,22 +1,23 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < N; i++) {
-            bw.write("*".repeat(i + 1));
-            bw.newLine();
+            sb.append("*".repeat(i + 1)).append("\n");
         }
 
-        sc.close();
-        bw.flush();
+        bw.write(sb.toString());
         bw.close();
+        br.close();
     }
 }
